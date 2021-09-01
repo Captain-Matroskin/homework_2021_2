38153eb953d1324f0d72b462fa487edd89317775
Matroskin <matroskin771@mail.ru>
2021-09-01 18:43:27 +0300

    fix and changes JSDoc

--- source/set.js
+++ source/set.js
@@ -2,10 +2,10 @@
 
 /**
  * Функция, которая получает путь к вложенному свойству объекта и устанавливает значени в это свойство
- * @param object
- * @param key
- * @param data
- * @returns {*}
+ * @param {object} object - объект, в котором хотим установить значение свойства
+ * @param {string} key - сам путь свойства
+ * @param {number|string|boolean|bigint} data - само значение
+ * @returns {undefiend|*object} - возвращает объект с измененным знгчением или undefiend, если свойство не найдено
  */
 
 const set = (object, key, data) => {
@@ -22,12 +22,12 @@ const set = (object, key, data) => {
             return;
         }
 
-       if ( !(item in nestingObject) ) {
-           nestingObject[item] = {};
-       }
-       if (index < lengthKey - 1) {
-           nestingObject = nestingObject[item];
-       }
+        if ( !(item in nestingObject) ) {
+            nestingObject[item] = {};
+        }
+        if (index < lengthKey - 1) {
+            nestingObject = nestingObject[item];
+        }
     });
 
     nestingObject[arrKey[lengthKey - 1]] = data;
